e40849f4bb366a611b51e96773628e55faaa104f
Besorez <Besorez@ukr.net>
2015-09-29 19:47:26 +0300

    Добавил игровой обьект

--- Assets/Script/move.cs
+++ Assets/Script/move.cs
@@ -1,15 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class move : MonoBehaviour {
+public class move : MonoBehaviour
+{
+    public int stepLength;
+    public GameObject player;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
