5175d165e3a50320a02f4d8315f33b4c03071711
Besorez <Besorez@ukr.net>
2015-09-30 14:52:09 +0300

    Движение персонажа по вектору направления

--- Assets/Script/move.cs
+++ Assets/Script/move.cs
@@ -6,33 +6,93 @@ public class move : MonoBehaviour
     public int stepLength;
     public GameObject player;
 	public int camSpeed;
+	public Vector3 Direction;
+
+	private int Rotation;
+
     // Use this for initialization
     void Start()
     {
-
+		Direction = new Vector3 (0, 0, stepLength);
     }
+
+	void ChangeDirection(bool direction)
+	{
+		if (direction == true) {
+			if (Direction.z == stepLength) {
+				Direction = new Vector3 (stepLength, 0, 0);
+				return;
+			}
+
+			if (Direction.x == stepLength) {
+				Direction = new Vector3 (0, 0, -stepLength);
+				return;
+			}
+
+			if (Direction.z == -stepLength) {
+				Direction = new Vector3 (-stepLength, 0, 0);
+				return;
+			}
+
+			if (Direction.x == -stepLength) {
+				Direction = new Vector3 (0, 0, stepLength);
+				return;
+			}
+		}
+
+		if (direction == false) {
+			if (Direction.z == stepLength) {
+				Direction = new Vector3 (-stepLength, 0, 0);
+				return;
+			}
+
+			if (Direction.x == stepLength) {
+				Direction = new Vector3 (0, 0, stepLength);
+				return;
+			}
+
+			if (Direction.z == -stepLength) {
+				Direction = new Vector3 (stepLength, 0, 0);
+				return;
+			}
+
+			if (Direction.x == -stepLength) {
+				Direction = new Vector3 (0, 0, -stepLength);
+				return;
+			}
+		}
+	}
 
     // Update is called once per frame
     void Update()
     {
 		if ( Input.GetKeyDown(KeyCode.W)) {
-			player.transform.position += new Vector3(0,0,stepLength);
+			player.transform.position += Direction;
 			Debug.Log("Произошло движение персонажа вперед");
 		}
 		else if ( Input.GetKeyDown(KeyCode.S)) {
-			player.transform.position -= new Vector3(0,0,stepLength);
+			player.transform.position -= Direction;
 			Debug.Log("Произошло движение персонажа назад");
 		}
-		else if ( Input.GetKeyDown(KeyCode.A)) {
-			// player.transform.position += new Vector3(stepLength,0,0);
-			//player.transform.rotation = new Quaternion(0,45,0,0);
-			Camera.main.transform.Rotate(0, camSpeed *Time.deltaTime, 0); //Крутим по оси Y
-
-			Debug.Log("Произошло движение персонажа влево");
-		} 
 		else if ( Input.GetKeyDown(KeyCode.D)) {
-			player.transform.position -= new Vector3(stepLength,0,0);
-			Debug.Log("Произошло движение персонажа вправо");
+			Rotation += 90;
+			if (Rotation == 360)
+			{
+				Rotation = 0;
+			}
+			player.transform.rotation = Quaternion.Euler(0,Rotation,0);
+			ChangeDirection(true);
+			Debug.Log("Произошел поворот персонажа вправо");
+		} 
+		else if ( Input.GetKeyDown(KeyCode.A)) {
+			if (Rotation == 0) {
+				Rotation = 360;
+			}
+			Rotation -= 90;
+
+			player.transform.rotation = Quaternion.Euler(0,Rotation,0);
+			ChangeDirection(false);
+			Debug.Log("Произошло поворот персонажа влево");
 		} 
     }
 }
